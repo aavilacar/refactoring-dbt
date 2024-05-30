135ff736d9b58ce328132946b96fc447529cbc86
Alejandro Avila Carrión <alejandro.avila@sdggroup.com>
2024-05-30 10:57:47 +0000

    Remove join in favor of window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -48,29 +48,30 @@ with
     final as (
         select
             p.*,
+
+            -- Sales transaction sequence
             row_number() over (order by p.order_id) as transaction_seq,
+
+            -- Customer sales sequence
             row_number() over (
                 partition by customer_id order by p.order_id
             ) as customer_sales_seq,
+
+            -- New vs returning customer
             case
                 when c.first_order_date = p.order_placed_at then 'new' else 'return'
             end as nvsr,
-            x.clv_bad as customer_lifetime_value,
+
+            -- Customer lifetime value
+            sum(total_amount_paid) over (
+                partition by p.customer_id order by p.order_placed_at
+            ) as customer_lifetime_value,
+
+            -- First day of sale
             c.first_order_date as fdos
+
         from paid_orders p
         left join customer_orders as c using (customer_id)
-        left outer join
-            (
-                select p.order_id, sum(t2.total_amount_paid) as clv_bad
-                from paid_orders p
-                left join
-                    paid_orders t2
-                    on p.customer_id = t2.customer_id
-                    and p.order_id >= t2.order_id
-                group by 1
-                order by p.order_id
-            ) x
-            on x.order_id = p.order_id
         order by order_id
     )
 
