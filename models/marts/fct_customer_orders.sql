519e34c5932dadaedd0696b6a4c2b6251feb41df
Alejandro Avila Carrión <alejandro.avila@sdggroup.com>
2024-05-30 11:09:00 +0000

    Prefer fully qualified table names and references

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -25,53 +25,64 @@ with
             orders.user_id as customer_id,
             orders.order_date as order_placed_at,
             orders.status as order_status,
-            p.total_amount_paid,
-            p.payment_finalized_date,
-            c.first_name as customer_first_name,
-            c.last_name as customer_last_name
+            completed_payments.total_amount_paid,
+            completed_payments.payment_finalized_date,
+            customers.first_name as customer_first_name,
+            customers.last_name as customer_last_name
         from orders
-        left join completed_payments p on orders.id = p.order_id
-        left join customers c on orders.user_id = c.id
+        left join completed_payments on orders.id = completed_payments.order_id
+        left join customers on orders.user_id = customers.id
     ),
 
     customer_orders as (
         select
-            c.id as customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
+            customers.id as customer_id,
+            min(orders.order_date) as first_order_date,
+            max(orders.order_date) as most_recent_order_date,
             count(orders.id) as number_of_orders
-        from customers c
-        left join orders on orders.user_id = c.id
+        from customers
+        left join orders on customers.id = orders.user_id
         group by 1
     ),
 
     final as (
         select
-            p.*,
+            paid_orders.order_id,
+            paid_orders.customer_id,
+            paid_orders.order_placed_at,
+            paid_orders.order_status,
+            paid_orders.total_amount_paid,
+            paid_orders.payment_finalized_date,
+            paid_orders.customer_first_name,
+            paid_orders.customer_last_name,
 
             -- Sales transaction sequence
-            row_number() over (order by p.order_id) as transaction_seq,
+            row_number() over (order by paid_orders.order_id) as transaction_seq,
 
             -- Customer sales sequence
             row_number() over (
-                partition by customer_id order by p.order_id
+                partition by paid_orders.customer_id order by paid_orders.order_id
             ) as customer_sales_seq,
 
             -- New vs returning customer
             case
-                when c.first_order_date = p.order_placed_at then 'new' else 'return'
+                when customer_orders.first_order_date = paid_orders.order_placed_at
+                then 'new'
+                else 'return'
             end as nvsr,
 
             -- Customer lifetime value
-            sum(total_amount_paid) over (
-                partition by p.customer_id order by p.order_placed_at
+            sum(paid_orders.total_amount_paid) over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at
             ) as customer_lifetime_value,
 
             -- First day of sale
-            c.first_order_date as fdos
+            customer_orders.first_order_date as fdos
 
-        from paid_orders p
-        left join customer_orders as c using (customer_id)
+        from paid_orders
+        left join
+            customer_orders on paid_orders.customer_id = customer_orders.customer_id
         order by order_id
     )
 
